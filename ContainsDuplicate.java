cd2757e5c75a9117cb6af4b474830f9e187ec904
NagaRaju1991 <vandanapu.raju@gmail.com>
2023-07-02 14:21:20 +0530

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,5 +1,7 @@
+// https://leetcode.com/problems/contains-duplicate/
+
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
+    /public boolean containsDuplicate(int[] nums) {
         Set<Integer> numbers = new HashSet<>();
         for(int i : nums) {
             if(!numbers.add(i)) {
